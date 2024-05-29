826106ea977d015255524eeb48d37dc91d80b173
faraui <rusya.abbas@gmail.com>
2024-05-29 22:51:32 +0200

    First commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ bot = telebot.TeleBot(config.token)
 @bot.message_handler(content_types=['text', 'audio', 'document', 'photo', 'sticker', 'video', 'video_note', 'voice', 'location', 'contact'])
 def on_message(message):
     if message.text == '/start':
-        bot.send_message(message.chat.id, "проверОчка бота")
+        bot.send_message(message.chat.id, "Bot check")
     else:
         if message.chat.id != config.target_chat:
             is_photo = message.content_type in ('photo', 'video', 'audio', 'document', 'voice')
@@ -27,16 +27,16 @@ def on_message(message):
                 users[message.chat.id].limit -= 1
                 forward(message, users[message.chat.id].limit)
             else:
-                print(f'W: Сообщение до таймаута от tg://user?id={message.chat.id}')
+                print(f'Message before timeout from tg://user?id={message.chat.id}')
                 users[message.chat.id].limit = 0
-                bot.send_message(message.chat.id, 'Превышен лимит сообщений. Ещё терпеть: {0:.1f}сек'.format(
+                bot.send_message(message.chat.id, 'Messages limit exceeded. Wait for {0:.1f}сек'.format(
                     config.timeout + users[message.chat.id].last_t - time.time()
                 ))
 
 def forward(message, limit = None):
     users[message.chat.id].last_t = time.time()
     bot.forward_message(config.target_chat, message.chat.id, message.message_id)
-    bot.send_message(message.chat.id, f'Отправлено: {11 - limit}. Максимум 10 за раз.' if limit else "Спасибки")
+    bot.send_message(message.chat.id, f'Sended {11 - limit}. Maximum 10 at a time.' if limit else "Thanks")
 
 print("Running...")
 bot.infinity_polling()
